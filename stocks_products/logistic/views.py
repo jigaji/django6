5a5d22078b652ee301d8a103555692f1c5f803b5
Saglara Tserenova <ts.saglara@gmail.com>
2021-12-13 17:15:02 +0300

    second commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,21 @@
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    filterset_fields = ['title', 'description']
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['products']
+    filterset_fields = ['product']
