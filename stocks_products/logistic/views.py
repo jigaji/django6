4a4d0b3e18a4703e21d8a9de118cd2ecbd634534
Saglara Tserenova <ts.saglara@gmail.com>
2021-12-22 01:36:26 +0300

    updated

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
@@ -8,7 +9,7 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filterset_fields = ['title', 'description']
     search_fields = ['title', 'description']
 
@@ -16,5 +17,5 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['products__title']
